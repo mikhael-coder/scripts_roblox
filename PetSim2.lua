c3b86c6e70d4981179f987b192c6d303e4d4518e
egorpisch23 <135999238+egorpisch23@users.noreply.github.com>
2024-01-05 05:12:09 +0300

    Create PetSim2.lua

---
+++ PetSim2.lua
@@ -0,0 +1,126 @@
+local ClassNames = {"Script", "ModuleScript", "LocalScript"}
+local PathScript = ""
+print("")
+print("")
+
+local function printTable(t, indent, maxDepth, currentDepth)
+    indent = indent or 0
+    maxDepth = maxDepth or math.huge -- максимальная глубина по умолчанию (бесконечность)
+    currentDepth = currentDepth or 0
+
+    local spaces = string.rep(" ", indent)
+    if type(t) == "table" then
+        for k, v in pairs(t) do
+            local keyStr = tostring(k)
+            local valueStr = tostring(v)
+
+            if type(v) == "table" and currentDepth < maxDepth then
+                if keyStr:find("Triple") then
+                    print("Найден ключ Triple в скрипте " .. PathScript)
+                    print(keyStr)
+                end
+                if keyStr:find("108819920") then
+                    print("Найден ключ 108819920 в скрипте " .. PathScript)
+                    print(keyStr)
+                end
+                printTable(v, indent + 4, maxDepth, currentDepth + 1)
+            elseif type(v) == "function" and currentDepth < maxDepth then
+                if keyStr:find("Triple") then
+                    print("Найден ключ Triple в скрипте " .. PathScript)
+                    print(keyStr)
+                end
+                if keyStr:find("108819920") then
+                    print("Найден ключ 108819920 в скрипте " .. PathScript)
+                    print(keyStr)
+                end
+                printTable(v, indent + 4, maxDepth, currentDepth + 1)
+            else
+                if keyStr:find("Triple") then
+                    print("Найден ключ Triple в скрипте " .. PathScript)
+                    print(keyStr .. ": " .. valueStr)
+                end
+                if valueStr:find("Triple") then
+                    print("Найдено значение Triple в скрипте " .. PathScript)
+                    print(keyStr .. ": " .. valueStr)
+                end
+                if keyStr:find("108819920") then
+                    print("Найден ключ 108819920 в скрипте " .. PathScript)
+                    print(keyStr .. ": " .. valueStr)
+                end
+                if valueStr:find("108819920") then
+                    print("Найдено значение 108819920 в скрипте " .. PathScript)
+                    print(keyStr .. ": " .. valueStr)
+                end
+            end
+        end
+    elseif type(t) == "function" then
+        success, result = pcall(t)
+        if success then
+            if type(result) == "table" then
+                for k, v in pairs(result) do
+                    local keyStr = tostring(k)
+                    local valueStr = tostring(v)
+
+                    if type(v) == "table" and currentDepth < maxDepth then
+                        if keyStr:find("Triple") then
+                            print("Найден ключ Triple в скрипте " .. PathScript)
+                            print(keyStr)
+                        end
+                        if keyStr:find("108819920") then
+                            print("Найден ключ 108819920 в скрипте " .. PathScript)
+                            print(keyStr)
+                        end
+                        printTable(v, indent + 4, maxDepth, currentDepth + 1)
+                    elseif type(v) == "function" and currentDepth < maxDepth then
+                        if keyStr:find("Triple") then
+                            print("Найден ключ Triple в скрипте " .. PathScript)
+                            print(keyStr)
+                        end
+                        if keyStr:find("108819920") then
+                            print("Найден ключ 108819920 в скрипте " .. PathScript)
+                            print(keyStr)
+                        end
+                        printTable(v, indent + 4, maxDepth, currentDepth + 1)
+                    else
+                        if keyStr:find("Triple") then
+                            print("Найден ключ Triple в скрипте " .. PathScript)
+                            print(keyStr .. ": " .. valueStr)
+                        end
+                        if valueStr:find("Triple") then
+                            print("Найдено значение Triple в скрипте " .. PathScript)
+                            print(keyStr .. ": " .. valueStr)
+                        end
+                        if keyStr:find("108819920") then
+                            print("Найден ключ 108819920 в скрипте " .. PathScript)
+                            print(keyStr .. ": " .. valueStr)
+                        end
+                        if valueStr:find("108819920") then
+                            print("Найдено значение 108819920 в скрипте " .. PathScript)
+                            print(keyStr .. ": " .. valueStr)
+                        end
+                    end
+                end
+            elseif type(result) == "string" or type(result) == "number" then
+                result = tostring(result)
+                if result:find("Triple") then
+                    print("Найден результат функции Triple в скрипте " .. PathScript)
+                    print(keyStr)
+                end
+                if result:find("108819920") then
+                    print("Найден результат функции 108819920 в скрипте " .. PathScript)
+                    print(keyStr)
+                end
+            end
+        end
+    end
+end
+
+for k, v in ipairs(game:GetService("Workspace"):GetDescendants()) do
+    if table.find(ClassNames, v.ClassName) then
+        success, scriptormodule = pcall(require, v)
+        if success and scriptormodule ~= nil then
+            PathScript = v:GetFullName()
+            printTable(scriptormodule, 0, 8, 0)
+        end
+    end
+end
