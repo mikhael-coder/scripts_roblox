9a279699b7d4495872c589433cf13031db7c2e86
egorpisch23 <135999238+egorpisch23@users.noreply.github.com>
2024-01-05 04:18:48 +0300

    Update PetSim.lua

--- PetSim.lua
+++ PetSim.lua
@@ -3,34 +3,101 @@ local merchantRequest = networkService:WaitForChild("Merchant_RequestPurchase")
 local claimPlant = networkService:WaitForChild("Instancing_FireCustomFromClient")
 local player = game:GetService("Players").LocalPlayer
 
-local function BuyMerchant(Name, Pos, State, NameFun)
-    while true do
+_G.autoBuyRegularMerchant = false
+_G.autoBuyAdvancedMerchant = false
+_G.autoBuyGardenMerchant = false
+_G.autoBuySnowMerchant = false
+
+local function AutoBuyRegularMerchant()
+    while _G.autoBuyRegularMerchant do
         playerHumanoid = player.Character
         if playerHumanoid then
             RootPart = playerHumanoid:FindFirstChild("HumanoidRootPart")
             if RootPart then
                 CFramePart = RootPart.Position
-                RootPart.CFrame = Pos
+                RootPart.CFrame = CFrame.new(368.82782, 17.6504326, 543.36051)
                 wait(0.1)
-                if NameFun == "Sno" then
-                    for i = 2,6 do
-                        for j = 1,10 do
-                             merchantRequest:InvokeServer(Name, i)
-                             wait(0.001)
-                        end
-                        wait(0.2) 
+                for i = 1,6 do
+                    for j = 1,10 do
+                        merchantRequest:InvokeServer("RegularMerchant", i)
+                        wait(0.001)
                     end
-                else
-                    for i = 1,6 do
-                        for j = 1,10 do
-                             merchantRequest:InvokeServer(Name, i)
-                             wait(0.001)
-                        end
-                        wait(0.2)                    
-                    end
-                    wait(0.1)
-                    RootPart.CFrame = CFrame.new(CFramePart)
+                    wait(0.2)                    
                 end
+                wait(0.1)
+                RootPart.CFrame = CFrame.new(CFramePart)
+            end
+        end
+        wait(100)
+    end
+end
+
+local function AutoBuyAdvancedMerchant()
+    while _G.autoBuyAdvancedMerchant do
+        playerHumanoid = player.Character
+        if playerHumanoid then
+            RootPart = playerHumanoid:FindFirstChild("HumanoidRootPart")
+            if RootPart then
+                CFramePart = RootPart.Position
+                RootPart.CFrame = CFrame.new(818.6481323, 17.66573, 1539.2429199)
+                wait(0.1)
+                for i = 1,6 do
+                    for j = 1,10 do
+                        merchantRequest:InvokeServer("AdvancedMerchant", i)
+                        wait(0.001)
+                    end
+                    wait(0.2)                    
+                end
+                wait(0.1)
+                RootPart.CFrame = CFrame.new(CFramePart)
+            end
+        end
+        wait(100)
+    end
+end
+
+local function AutoBuyGardenMerchant()
+    while _G.autoBuyGardenMerchant do
+        playerHumanoid = player.Character
+        if playerHumanoid then
+            RootPart = playerHumanoid:FindFirstChild("HumanoidRootPart")
+            if RootPart then
+                CFramePart = RootPart.Position
+                RootPart.CFrame = CFrame.new(259.746124267, 17.6442584, 2073.28051757)
+                wait(0.1)
+                for i = 1,6 do
+                    for j = 1,10 do
+                        merchantRequest:InvokeServer("GardenMerchant", i)
+                        wait(0.001)
+                    end
+                    wait(0.2)                    
+                end
+                wait(0.1)
+                RootPart.CFrame = CFrame.new(CFramePart)
+            end
+        end
+        wait(100)
+    end
+end
+
+local function AutoBuySnowMerchant()
+    while _G.autoBuySnowMerchant do
+        playerHumanoid = player.Character
+        if playerHumanoid then
+            RootPart = playerHumanoid:FindFirstChild("HumanoidRootPart")
+            if RootPart then
+                CFramePart = RootPart.Position
+                RootPart.CFrame = CFrame.new(1258.35363769531, 17.64980506, 2649.2119140625)
+                wait(0.1)
+                for i = 2,6 do
+                    for j = 1,10 do
+                        merchantRequest:InvokeServer("SnowMerchant", i)
+                        wait(0.001)
+                    end
+                    wait(0.2)                    
+                end
+                wait(0.1)
+                RootPart.CFrame = CFrame.new(CFramePart)
             end
         end
         wait(100)
@@ -50,3 +117,41 @@ local Window = OrionLib:MakeWindow({Name = "Pet Simulator 99", HidePremium = fal
         local Section1 = TabAutoDo:AddSection({
 	        Name = "Merchant"
         })
+
+            TabAutoDo:AddToggle({
+	        Name = "AutoBuy Regular Merchant",
+	        Default = false,
+	        Callback = function(Value)
+		    _G.autoBuyRegularMerchant = Value
+		    AutoBuyRegularMerchant()
+	        end    
+            })
+
+            TabAutoDo:AddToggle({
+	        Name = "AutoBuy Advanced Merchant",
+	        Default = false,
+	        Callback = function(Value)
+		    _G.autoBuyAdvancedMerchant = Value
+		    AutoBuyAdvancedMerchant()
+	        end    
+            })
+
+            TabAutoDo:AddToggle({
+	        Name = "AutoBuy Garden Merchant",
+	        Default = false,
+	        Callback = function(Value)
+		    _G.autoBuyGardenMerchant = Value
+		    AutoBuyGardenMerchant()
+	        end    
+            })
+
+            TabAutoDo:AddToggle({
+	        Name = "AutoBuy Snow Merchant",
+	        Default = false,
+	        Callback = function(Value)
+		    _G.autoBuySnowMerchant = Value
+		    AutoBuySnowMerchant()
+	        end    
+            })
+
+OrionLib:Init()
