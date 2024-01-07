537460faccb2c194a8f4f6c1d1b33faaa391c803
egorpisch23 <135999238+egorpisch23@users.noreply.github.com>
2024-01-07 19:02:23 +0300

    Update PetSim2.lua

--- PetSim2.lua
+++ PetSim2.lua
@@ -1,56 +1,19 @@
-local ClassNames = {"ModuleScript"}
 local PathScript = ""
 print("")
 print("")
 
 local function printTable(t, indent, maxDepth, currentDepth)
     indent = indent or 0
-    maxDepth = maxDepth or math.huge -- максимальная глубина по умолчанию (бесконечность)
+    maxDepth = maxDepth or math.huge
     currentDepth = currentDepth or 0
 
     local spaces = string.rep(" ", indent)
     if type(t) == "table" then
         for k, v in pairs(t) do
-            local keyStr = tostring(k)
-            local valueStr = tostring(v)
-
             if type(v) == "table" and currentDepth < maxDepth then
-                if keyStr:find("Triple") then
-                    print("Найден ключ Triple в скрипте " .. PathScript)
-                    print(keyStr)
-                end
-                if keyStr:find("108819920") then
-                    print("Найден ключ 108819920 в скрипте " .. PathScript)
-                    print(keyStr)
-                end
                 printTable(v, indent + 4, maxDepth, currentDepth + 1)
             elseif type(v) == "function" and currentDepth < maxDepth then
-                if keyStr:find("Triple") then
-                    print("Найден ключ Triple в скрипте " .. PathScript)
-                    print(keyStr)
-                end
-                if keyStr:find("108819920") then
-                    print("Найден ключ 108819920 в скрипте " .. PathScript)
-                    print(keyStr)
-                end
                 printTable(v, indent + 4, maxDepth, currentDepth + 1)
-            else
-                if keyStr:find("Triple") then
-                    print("Найден ключ Triple в скрипте " .. PathScript)
-                    print(keyStr .. ": " .. valueStr)
-                end
-                if valueStr:find("Triple") then
-                    print("Найдено значение Triple в скрипте " .. PathScript)
-                    print(keyStr .. ": " .. valueStr)
-                end
-                if keyStr:find("108819920") then
-                    print("Найден ключ 108819920 в скрипте " .. PathScript)
-                    print(keyStr .. ": " .. valueStr)
-                end
-                if valueStr:find("108819920") then
-                    print("Найдено значение 108819920 в скрипте " .. PathScript)
-                    print(keyStr .. ": " .. valueStr)
-                end
             end
         end
     elseif type(t) == "function" then
@@ -58,57 +21,11 @@ local function printTable(t, indent, maxDepth, currentDepth)
         if success then
             if type(result) == "table" then
                 for k, v in pairs(result) do
-                    local keyStr = tostring(k)
-                    local valueStr = tostring(v)
-
                     if type(v) == "table" and currentDepth < maxDepth then
-                        if keyStr:find("Triple") then
-                            print("Найден ключ Triple в скрипте " .. PathScript)
-                            print(keyStr)
-                        end
-                        if keyStr:find("108819920") then
-                            print("Найден ключ 108819920 в скрипте " .. PathScript)
-                            print(keyStr)
-                        end
                         printTable(v, indent + 4, maxDepth, currentDepth + 1)
                     elseif type(v) == "function" and currentDepth < maxDepth then
-                        if keyStr:find("Triple") then
-                            print("Найден ключ Triple в скрипте " .. PathScript)
-                            print(keyStr)
-                        end
-                        if keyStr:find("108819920") then
-                            print("Найден ключ 108819920 в скрипте " .. PathScript)
-                            print(keyStr)
-                        end
                         printTable(v, indent + 4, maxDepth, currentDepth + 1)
-                    else
-                        if keyStr:find("Triple") then
-                            print("Найден ключ Triple в скрипте " .. PathScript)
-                            print(keyStr .. ": " .. valueStr)
-                        end
-                        if valueStr:find("Triple") then
-                            print("Найдено значение Triple в скрипте " .. PathScript)
-                            print(keyStr .. ": " .. valueStr)
-                        end
-                        if keyStr:find("108819920") then
-                            print("Найден ключ 108819920 в скрипте " .. PathScript)
-                            print(keyStr .. ": " .. valueStr)
-                        end
-                        if valueStr:find("108819920") then
-                            print("Найдено значение 108819920 в скрипте " .. PathScript)
-                            print(keyStr .. ": " .. valueStr)
-                        end
                     end
-                end
-            elseif type(result) == "string" or type(result) == "number" then
-                result = tostring(result)
-                if result:find("Triple") then
-                    print("Найден результат функции Triple в скрипте " .. PathScript)
-                    print(keyStr)
-                end
-                if result:find("108819920") then
-                    print("Найден результат функции 108819920 в скрипте " .. PathScript)
-                    print(keyStr)
                 end
             end
         end
@@ -116,10 +33,10 @@ local function printTable(t, indent, maxDepth, currentDepth)
 end
 
 for k, v in ipairs(game:GetService("ReplicatedStorage").Network:GetDescendants()) do
-    if table.find(ClassNames, v.ClassName) then
+    if v.ClassName == "ModuleScript" then
         success, scriptormodule = pcall(require, v)
         if success and scriptormodule ~= nil then
-            print(v:GetFullName())
+            print(v.Name)
             PathScript = v:GetFullName()
             printTable(scriptormodule, 0, 4, 0)
             wait(2)
