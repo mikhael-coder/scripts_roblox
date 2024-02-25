41c5e2cc762c5420ed33634af98ddc67f4026f84
egorpisch23 <135999238+egorpisch23@users.noreply.github.com>
2024-02-25 22:14:27 +0300

    Update PetProAssist.lua

--- PetProAssist.lua
+++ PetProAssist.lua
@@ -1,18 +1,27 @@
+-- Library of GUI Hub
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "Pet Simulator 99", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroEnabled = true, IntroText = "Hi! It's best script for Pet Sim 99!"})
 
+-- 1 Level
 local ws = game:GetService("Workspace")
 local rs = game:GetService("ReplicatedStorage")
 
+-- 2 Level
 local th = ws["__THINGS"]
 local nw = rs.Network
 
+-- 3 Level
 local bu = th.Breakables
 local b_pdd = nw["Breakables_PlayerDealDamage"]
 
+-- Removing unnecessary objects that interfere with the script
+bu.Highlight:Destroy()
+
+-- Global variables controlling the operation of repeating functions
 _G.autoTap = false
 
+-- Functions
 local function FS(fun, args)
     fun:FireServer(unpack(args))
 end
@@ -23,27 +32,29 @@ end
 
 local function AutoTap()
     while _G.autoTap do
-        bu.Highlight:Destroy()
         for i,v in ipairs(bu:GetChildren()) do
             if not _G.autoTap then return end
             tab = {[1] = v.Name}
             FS(b_pdd, tab)
-            wait(0.001)
+            wait(0.000000001)
         end
-        wait(0.001)
+        wait(0.000000001)
     end
 end
 
+    -- Tabs
     local TabAutoFarm = Window:MakeTab({
 	    Name = "AutoFarm",
 	    Icon = "rbxassetid://12767693169",
 	    PremiumOnly = false
     })
 
+        -- Sections
         local STAF = TabAutoFarm:AddSection({
 	        Name = "Farm"
         })
 
+            -- Toggles
             TabAutoFarm:AddToggle({
 	            Name = "AutoTap Buildings",
 	            Default = false,
